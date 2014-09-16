e39a03ac3c1bbe9ee0d75dbb901a795be79471df
robertsone <rufrrufr@yahoo.com>
2014-09-16 08:40:19 -0400

    1+2

---
+++ Chapter 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter any numbers you want,then type 999 to sum them");
+            int sum = 0;
+            while (true)
+            {
+                int num = (Convert.ToInt32(Console.ReadLine()));
+                if (num == 999)
+                    break;
+                else
+                    sum += num;
+
+                
+            }
+            System.Console.WriteLine("Your sum is " + sum);
+        }
+    }
+}
---
+++ GetVowel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Type in one letter!");
+            while (true)
+            {
+                string letter =(System.Console.ReadLine()).ToLower();
+                if (letter == "a" || letter == "e" || letter == "o" || letter == "u" || letter == "i")
+                    System.Console.WriteLine("ok");
+                else if (letter == "!")
+                    break;
+                else
+                    System.Console.WriteLine("NO!");
+
+            }
+        }
+    }
+}
