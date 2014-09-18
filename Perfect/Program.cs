d7a68f31a984111939fb97381c778b11b36b99c4
robertsone <rufrrufr@yahoo.com>
2014-09-18 08:10:42 -0400

    one line

--- Perfect/Program.cs
+++ Perfect/Program.cs
@@ -4,30 +4,4 @@ using System.Linq;
 using System.Text;
 
 namespace Perfect
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int Perfect = 0;
-            int count = 1;
-
-            while (true)
-            {
-                count += 1;
-                int count_count = count-1;
-                int count_sum = 0;
-                while (true)
-                {
-                    if (count % count_count == 0)
-                        count_sum += count_count;
-                    count_count -= 1;
-                    if (count_count == 0)
-                        break;
-                }
-                if (count_sum == count)
-                    System.Console.WriteLine(Perfect);
-            }
-        }
-    }
-}
+{class Program{static void Main(string[] args){int Num_counter = 5; int num_sum = 0; while (true){Num_counter += 1; num_sum = 0; int Number_counter_in_number_counter = Num_counter - 1; while (true){Number_counter_in_number_counter -= 1; if (Number_counter_in_number_counter == 0) if (Num_counter == num_sum){System.Console.WriteLine(num_sum);break;}else{break;}if (Num_counter % Number_counter_in_number_counter == 0){num_sum += Number_counter_in_number_counter;}}}}}}
